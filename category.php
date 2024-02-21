644399b5e26fba3925de7ec937017b5274bd0403
Kuswandanu Kusuma Wicaksana <kuswandanu@gmail.com>
2024-02-21 14:11:11 +0700

    fix link PostImage

--- category.php
+++ category.php
@@ -60,7 +60,7 @@ include('includes/config.php');
         $total_rows = mysqli_fetch_array($result)[0];
         $total_pages = ceil($total_rows / $no_of_records_per_page);
 
-        $query=mysqli_query($con,"select offline_posts.id as pid,offline_posts.title as posttitle,tblcategory.CategoryName as category,offline_posts.description as description,offline_posts.posting_date as postingdate from offline_posts JOIN offline_post_images ON offline_posts.id = offline_post_images.post_id left join tblcategory on tblcategory.id=offline_posts.category_id where offline_posts.category_id='".$_SESSION['catid']."' and offline_posts.active=1 order by offline_posts.id desc LIMIT $offset, $no_of_records_per_page");
+        $query=mysqli_query($con,"select offline_posts.id as pid,offline_posts.title as posttitle,tblcategory.CategoryName as category,offline_posts.description as description,offline_posts.posting_date as postingdate, offline_post_images.url as PostImage from offline_posts JOIN offline_post_images ON offline_posts.id = offline_post_images.post_id left join tblcategory on tblcategory.id=offline_posts.category_id where offline_posts.category_id='".$_SESSION['catid']."' and offline_posts.active=1 order by offline_posts.id desc LIMIT $offset, $no_of_records_per_page");
         // $query=mysqli_query($con,"select tblposts.id as pid,tblposts.PostTitle as posttitle,tblposts.PostImage,tblcategory.CategoryName as category,tblsubcategory.Subcategory as subcategory,tblposts.PostDetails as postdetails,tblposts.PostingDate as postingdate,tblposts.PostUrl as url from tblposts left join tblcategory on tblcategory.id=tblposts.CategoryId left join  tblsubcategory on  tblsubcategory.SubCategoryId=tblposts.SubCategoryId where tblposts.CategoryId='".$_SESSION['catid']."' and tblposts.Is_Active=1 order by tblposts.id desc LIMIT $offset, $no_of_records_per_page");
 
 $rowcount=mysqli_num_rows($query);
@@ -75,7 +75,7 @@ while ($row=mysqli_fetch_array($query)) {
 ?>
 <h1><?php echo htmlentities($row['category']);?> News</h1>
           <div class="card mb-4">
-       <img class="card-img-top" src="admin/postimages/<?php echo htmlentities($row['PostImage']);?>" alt="<?php echo htmlentities($row['posttitle']);?>">
+       <img class="card-img-top" src="admin/<?php echo htmlentities($row['PostImage']);?>" alt="<?php echo htmlentities($row['posttitle']);?>">
             <div class="card-body">
               <h2 class="card-title"><?php echo htmlentities($row['posttitle']);?></h2>
            
